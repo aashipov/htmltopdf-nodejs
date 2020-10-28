9c35e2f56913bf73bdcbba9d3b526263289110ab
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2020-10-28 16:31:21 +0300

    disable-smart-shrinking

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ const unknownConverter = (res, printerOptions) => {
 }
 
 const viaWkhtmltopdf = async (res, printerOptions) => {
-  let osCmd = spawn(wkhtmltopdf, ['--enable-local-file-access', '--print-media-type', '--no-stop-slow-scripts',
+  let osCmd = spawn(wkhtmltopdf, ['--enable-local-file-access', '--print-media-type', '--no-stop-slow-scripts', '--disable-smart-shrinking',
     '--margin-bottom', printerOptions.bottom, '--margin-left', printerOptions.left, '--margin-right', printerOptions.right, '--margin-top', printerOptions.top,
     '--page-width', printerOptions.paperSize.widthMm, '--page-height', printerOptions.paperSize.heightMm, '--orientation', printerOptions.orientation,
     indexHtml, resultPdf], {
