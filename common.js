b174f8b73b9af687d4c169298d4413f68b211942
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-02-05 11:48:05 +0300

    msg

--- common.js
+++ common.js
@@ -15,6 +15,21 @@ export const chromium = 'chromium';
 export const wkhtmltopdf = 'wkhtmltopdf';
 export const landscape = 'landscape';
 
+const portrait = 'portrait';
+const file = 'file';
+const finish = 'finish';
+const contentType = 'Content-Type';
+const contentLenght = 'Content-Length';
+const applicationPdf = 'application/pdf';
+const applicationJsonUtf8 = 'application/json;charset=utf-8';
+const statusUp = '{"status":"UP"}';
+const tmp = 'tmp';
+const noIndexHtml = 'No ' + indexHtml;
+const unknownConverterText = 'Unknown converter';
+
+const http500 = 500;
+const http200 = 200;
+
 const A4 = new PaperSize('210', '297');
 const A3 = new PaperSize('297', '420');
 const a3 = 'a3';
@@ -25,7 +40,7 @@ const top = 'top';
 const bottom = 'bottom';
 const oneOrMoreDigitsRe = new RegExp(/\d+/);
 
-const tmpDir = path.join(path.resolve(), '/tmp/');
+const tmpDir = path.join(path.resolve(), tmp);
 
 const fillMarginNameReMap = () => {
     let m = new Map();
@@ -60,27 +75,34 @@ const mkdirSync = (dir) => {
 
 const teapot = (res, printerOptions) => {
     res.statusCode = 418;
-    res.write('No index.html');
+    res.write(noIndexHtml);
     fs.remove(printerOptions.workDir);
     res.end();
 }
 
 const unknownConverter = (res, printerOptions) => {
-    res.statusCode = 500;
-    res.write('Unknown converter');
+    res.statusCode = http500;
+    res.write(unknownConverterText);
     fs.remove(printerOptions.workDir);
     res.end();
 }
 
 export const healthCheck = (res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    res.write('{"status":"UP"}');
+    res.statusCode = http200;
+    res.setHeader(contentType, applicationJsonUtf8);
+    res.write(statusUp);
     res.end();
 }
 
-export const htmlToPdf = async (req, res) => {
-    let printerOptions = new PrinterOptions(path.join(tmpDir, '' + Math.random() + '-' + Math.random()), [], req.url, A4, 'portrait', defaultMargin, defaultMargin, defaultMargin, defaultMargin);
+const buildPrinterOptions = (req) => {
+    let printerOptions =
+        new PrinterOptions(
+            path.join(tmpDir, '' + Math.random() + '-' + Math.random()),
+            [],
+            req.url,
+            A4,
+            portrait,
+            defaultMargin, defaultMargin, defaultMargin, defaultMargin);
     if (printerOptions.originalUrl.includes(a3)) {
         printerOptions.paperSize = A3;
     }
@@ -105,14 +127,19 @@ export const htmlToPdf = async (req, res) => {
             }
         }
     }
+    return printerOptions;
+}
+
+export const htmlToPdf = async (req, res) => {
+    let printerOptions = buildPrinterOptions(req);
     mkdirSync(printerOptions.workDir);
     let busboy = new Busboy({
         headers: req.headers
     });
     req.pipe(busboy);
     busboy
-        .on('file', (fieldname, file, filename) => receiveFiles(file, filename, printerOptions))
-        .on('finish', () => {
+        .on(file, (fieldname, file, filename) => receiveFiles(file, filename, printerOptions))
+        .on(finish, () => {
             if (isIndexHtml(printerOptions.fileNames)) {
                 if (printerOptions.originalUrl.includes(chromium)) {
                     viaPuppeteer(res, printerOptions);
@@ -134,8 +161,8 @@ export const setUp = () => {
 
 export const sendPdf = (response, currentPdfFile) => {
     response.writeHead(200, {
-        'Content-Type': 'application/pdf',
-        'Content-Length': fs.statSync(currentPdfFile).size
+        contentType: applicationPdf,
+        contentLenght: fs.statSync(currentPdfFile).size
     });
     let readStream = fs.createReadStream(currentPdfFile);
     readStream.pipe(response);
