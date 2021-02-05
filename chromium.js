65e7c5996b91fc534c813f923db48fe37f885eee
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-02-05 17:08:28 +0300

    msg

--- chromium.js
+++ chromium.js
@@ -8,23 +8,41 @@ const mm = 'mm';
 const browserLock = new ReadWriteLock();
 const browserTimeout = 30000;
 const defaultEvents = ['load', 'domcontentloaded', 'networkidle0', 'networkidle2'];
+// Linux and Linux in container only
+const chromiumPath = '/usr/bin/chromium';
+const chromiumArgs = ['--no-sandbox', '--disable-setuid-sandbox', '--disable-notifications', '--disable-geolocation', '--disable-infobars',
+    '--disable-session-crashed-bubble', '--disable-dev-shm-usage', '--disable-gpu', '--disable-translate', '--disable-extensions',
+    '--disable-background-networking', '--disable-sync', '--disable-default-apps', '--hide-scrollbars', '--metrics-recording-only',
+    '--mute-audio', '--no-first-run', '--unlimited-storage', '--safebrowsing-disable-auto-update', '--font-render-hinting=none'];
 let browser;
 
 const launchSuccess = () => console.log(`Chromium (re)started`);
 const launchFailure = (reason) => console.error(`Chromium failed to (re)start ${reason}`)
 const launchBrowser = async () => browser = await puppeteer.launch({
-    executablePath: '/usr/bin/chromium',
-    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-notifications', '--disable-geolocation', '--disable-infobars',
-        '--disable-session-crashed-bubble', '--disable-dev-shm-usage', '--disable-gpu', '--disable-translate', '--disable-extensions',
-        '--disable-background-networking', '--disable-sync', '--disable-default-apps', '--hide-scrollbars', '--metrics-recording-only',
-        '--mute-audio', '--no-first-run', '--unlimited-storage', '--safebrowsing-disable-auto-update', '--font-render-hinting=none']
+    executablePath: chromiumPath,
+    args: chromiumArgs
 });
 
 export const lauchChromiumHeadless = async () => {
     await launchBrowser().then(launchSuccess, launchFailure);
 }
 
-const buildFileUrl = (workDir) => (`file://${path.join(workDir, indexHtml)}`);
+const buildFileUrl = (workDir) => `file://${path.join(workDir, indexHtml)}`;
+const buildCurrentPdfFilePath = (printerOptions) => path.join(printerOptions.workDir, resultPdf);
+const buildPdfOpts = (printerOptions) => (
+    {
+        path: buildCurrentPdfFilePath(printerOptions),
+        width: printerOptions.paperSize.widthMm + mm,
+        height: printerOptions.paperSize.heightMm + mm,
+        landscape: printerOptions.orientation.includes(landscape),
+        margin: {
+            top: printerOptions.top + mm,
+            right: printerOptions.right + mm,
+            bottom: printerOptions.bottom + mm,
+            left: printerOptions.left + mm
+        }
+    }
+);
 
 export const viaPuppeteer = async (res, printerOptions) => {
     browserLock.readLock(async (release) => {
@@ -35,23 +53,11 @@ export const viaPuppeteer = async (res, printerOptions) => {
         await page.goto(buildFileUrl(printerOptions.workDir), {
             waitUntil: defaultEvents
         });
-        let currentPdfFile = path.join(printerOptions.workDir, resultPdf);
         // page.pdf() is currently supported only in headless mode.
         // @see https://bugs.chromium.org/p/chromium/issues/detail?id=753118
-        await page.pdf({
-            path: currentPdfFile,
-            width: printerOptions.paperSize.widthMm + mm,
-            height: printerOptions.paperSize.heightMm + mm,
-            landscape: printerOptions.orientation.includes(landscape),
-            margin: {
-                top: printerOptions.top + mm,
-                right: printerOptions.right + mm,
-                bottom: printerOptions.bottom + mm,
-                left: printerOptions.left + mm
-            }
-        });
+        await page.pdf(buildPdfOpts(printerOptions));
         await page.close();
-        sendPdf(res, currentPdfFile);
+        sendPdf(res, buildCurrentPdfFilePath(printerOptions));
         fs.remove(printerOptions.workDir);
         release();
         setTimeout(function () {
