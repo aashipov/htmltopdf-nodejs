e9099b1361033dbbfa6ec755a34f1381f7516434
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-02-08 13:00:47 +0300

    msg

--- test/farm/down.bash
+++ test/farm/down.bash
@@ -8,4 +8,6 @@ NODE_3=htmltopdf-nodejs3
 HAPROXY=htmltopdf-nodejs-haproxy
 NETWORK_NAME=htmltopdf-nodejs
 
-docker stop ${HAPROXY} ${NODE_1} ${NODE_2} ${NODE_3} ; docker rm ${HAPROXY} ${NODE_1} ${NODE_2} ${NODE_3} ; docker network rm ${NETWORK_NAME}
+docker stop ${HAPROXY} ${NODE_1} ${NODE_2} ${NODE_3}
+docker rm ${HAPROXY} ${NODE_1} ${NODE_2} ${NODE_3}
+docker network rm ${NETWORK_NAME}
