0ffd7d6dc23a96cba547d90da7fc6486493f6cc1
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-03-05 14:37:18 +0300

    build script

--- build-and-run.bash
+++ build-and-run.bash
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-#!/bin/bash
-
-# Dockerfile local testbed
 set -x
 
 #To ease copy-paste
-CONTAINER_NAME=htmltopdf-nodejs
+IMAGE="aashipov/htmltopdf-nodejs"
+CONTAINER_NAME=htmltopdf-dev
+TOP_COMMIT=$(git log --pretty=format:'%h' -n 1)
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-rm -rf node_modules/
-rm -rf package-lock.json
-
-docker build . --tag=${CONTAINER_NAME}
+docker build --file=Dockerfile --tag=${IMAGE}:latest --tag=${IMAGE}:${TOP_COMMIT} --tag=${IMAGE}:${CURRENT_BRANCH} .
 source ./down.bash
-docker run -d --name=${CONTAINER_NAME} --hostname=${CONTAINER_NAME} -p 8080:8080 ${CONTAINER_NAME}
+docker run -d --name=${CONTAINER_NAME} --hostname=${CONTAINER_NAME} -p 8080:8080 ${IMAGE}:${TOP_COMMIT}
+# Push VCS commit sha as docker hub tag to bypass nexus bug
+docker push ${IMAGE}:latest
+docker push ${IMAGE}:${TOP_COMMIT}
+docker push ${IMAGE}:${CURRENT_BRANCH}
