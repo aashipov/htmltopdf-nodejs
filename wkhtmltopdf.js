885c1995c381f21002f481b48ef9310cead0886b
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-03-23 17:57:57 +0300

    msg

--- wkhtmltopdf.js
+++ wkhtmltopdf.js
@@ -2,7 +2,7 @@ import { spawn, spawnSync } from 'child_process';
 import { indexHtml, resultPdf, sendPdf } from './handler.js';
 
 const wkhtmltopdfExecutable = 'wkhtmltopdf';
-const timeout = 600_000;
+const wkhtmltopdfTimeout = 600_000;
 
 const buildSpawnOptions = (printerOptions) => (['--enable-local-file-access', '--print-media-type', '--no-stop-slow-scripts', '--disable-smart-shrinking',
     '--margin-bottom', printerOptions.bottom, '--margin-left', printerOptions.left, '--margin-right', printerOptions.right, '--margin-top', printerOptions.top,
@@ -10,7 +10,7 @@ const buildSpawnOptions = (printerOptions) => (['--enable-local-file-access', '-
     indexHtml, resultPdf]);
 
 export const viaWkhtmltopdf = async (res, printerOptions) => {
-    let spawnSyncReturns = spawnSync(wkhtmltopdfExecutable, buildSpawnOptions(printerOptions), { cwd: printerOptions.workDir});
+    let spawnSyncReturns = spawnSync(wkhtmltopdfExecutable, buildSpawnOptions(printerOptions), { cwd: printerOptions.workDir, timeout: wkhtmltopdfTimeout});
     if (spawnSyncReturns.status === 0) {
         sendPdf(res, printerOptions);
         printerOptions.removeWorkDir();
